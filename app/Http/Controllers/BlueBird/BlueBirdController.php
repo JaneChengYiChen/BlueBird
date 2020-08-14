96b5fa84e539161f77d9fef121765449f7472854
janecheng <jane.zheng@leishan.com.tw>
2020-08-14 17:37:19 +0800

    echo bot

---
+++ app/Http/Controllers/BlueBird/BlueBirdController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\BlueBird;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use LINE\LINEBot;
+use Exception;
+use App\Http\Controllers\BlueBird\WhichBot;
+
+class BlueBirdController extends Controller
+{
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+        $this->whichBot = new WhichBot($this->request);
+    }
+
+    public function webhook()
+    {
+        $signature = $this->whichBot->signature(WhichBot::ECHO_BOT);
+        $lineBot = $this->whichBot->bot();
+
+        try {
+            $events = $lineBot->parseEventRequest($this->request->getContent(), $signature);
+            foreach ($events as $event) {
+                $replyToken = $event->getReplyToken();
+                $text = $event->getText();
+                $lineBot->replyText($replyToken, $text);
+            }
+        } catch (Exception $e) {
+            return;
+        }
+        return;
+    }
+}
---
+++ app/Http/Controllers/BlueBird/WhichBot.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\BlueBird;
+
+use LINE\LINEBot;
+use LINE\LINEBot\Constant\HTTPHeader;
+use LINE\LINEBot\SignatureValidator;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use Illuminate\Http\Request;
+
+class WhichBot
+{
+    const ECHO_BOT = 'echoBot';
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function signature($mode)
+    {
+        $this->mode = $mode;
+        $this->init();
+
+        $lineAccessToken = $this->lineAccessToken;
+        $lineChannelSecret =  $this->lineChannelSecret;
+
+        $signature = $this->request->headers->get(HTTPHeader::LINE_SIGNATURE);
+        if (!SignatureValidator::validateSignature($this->request->getContent(), $lineChannelSecret, $signature)) {
+            exit;
+        }
+
+        return $signature;
+    }
+
+    public function bot()
+    {
+        $httpClient = new CurlHTTPClient($this->lineAccessToken);
+        $lineBot = new LINEBot($httpClient, ['channelSecret' => $this->lineChannelSecret]);
+        
+        return $lineBot;
+    }
+
+    private function init()
+    {
+        switch ($this->mode) {
+            case WhichBot::ECHO_BOT:
+                $this->lineAccessToken = env('line_channel_access_token');
+                $this->lineChannelSecret = env('line_channel_secret');
+                break;
+            default:
+                $this->lineAccessToken = null;
+                $this->lineChannelSecret = null;
+                break;
+        }
+    }
+}
